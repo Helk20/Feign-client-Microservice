13cc3b0eb467b081d5d92c3f8703703b1d3bdfea
Helk20 <houdaelkoraini@gmail.com>
2023-12-11 00:35:51 +0100

    updated

--- Voiture/Voiture/src/main/java/org/ENSAJ/Repository/VoitureRepository.java
+++ Voiture/Voiture/src/main/java/org/ENSAJ/Repository/VoitureRepository.java
@@ -1,11 +1,15 @@
 package org.ENSAJ.Repository;
 
+import feign.Param;
 import org.ENSAJ.Model.Voiture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VoitureRepository extends JpaRepository<Voiture, Long> {
 
-//    @Query("SELECT v FROM Voiture v WHERE v.client.id = :clientId")
-//    List<Voiture> findByClientId(@Param("clientId") Long clientId);
+    @Query("SELECT v FROM Voiture v WHERE v.clientId = :clientId")
+    List<Voiture> findByClientId(@Param("clientId") Long clientId);
 
 }
--- Voiture/Voiture/src/main/java/org/ENSAJ/VoitureApplication.java
+++ Voiture/Voiture/src/main/java/org/ENSAJ/VoitureApplication.java
@@ -35,21 +35,7 @@ public class VoitureApplication {
 		Client clientById(@PathVariable Long id);
 	}
 
-	@Transactional
-	@Bean
-	CommandLineRunner initializeDatabase(VoitureRepository voitureRepository, ClientService clientService) {
-		return args -> {
-			Client c1 = clientService.clientById(2L);
-			Client c2 = clientService.clientById(1L);
-			System.out.println("**************************");
-			System.out.println("Id est :" + c1.getId());
-			System.out.println("Nom est :" + c1.getNom());
-			System.out.println("**************************");
 
-			voitureRepository.save(new Voiture(null, "Toyota", "A 25 333", "Corolla", 1L));
-			voitureRepository.save(new Voiture(null, "Renault", "B 6 3456", "Megane", 2L));
-			voitureRepository.save(new Voiture(null, "Peugeot", "A 55 4444", "301", 2L));
-		};
-	}
+
 
 }
